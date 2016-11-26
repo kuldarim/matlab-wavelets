831d9a6a2fd5adad872ab1f494ab3d27e6a1ac2c
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-11-26 01:58:54 -0800

    draw plost

--- wavelets.m
+++ wavelets.m
@@ -6,14 +6,19 @@ load mask;
 meth   = 'ezw';   % Method name
 wnames = {'haar', 'db1', 'sym2', 'coif1', 'bior1.1', 'rbio1.1'};
 
+
 for i = 1:6
   disp(char(wnames(i)));
   fprintf('CR BPP snr \n');
+  plotdata = [];
   % nbloop - number of loops
   for nbloop = 1:12
     [CR,BPP,snr] = get_compresion_rates(X, meth, char(wnames(i)), nbloop);
     fprintf('%3.2f, %3.2f, %3.2f \n', CR, BPP, snr);
+    plotdata(:,nbloop) = snr;
   end
+  %figure;plot(plotdata);
+  subplot(6,1,i), plot(plotdata), title(char(wnames(i)));
 end
 
 
