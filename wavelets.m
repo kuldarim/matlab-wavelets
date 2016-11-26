9bdccc76dc3aa7fab0ec79d51ff042311aded225
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-11-26 02:08:10 -0800

    changed

--- wavelets.m
+++ wavelets.m
@@ -5,20 +5,22 @@ load mask;
 
 meth   = 'ezw';   % Method name
 wnames = {'haar', 'db1', 'sym2', 'coif1', 'bior1.1', 'rbio1.1'};
+nbloops = [1,2,3,5,6,7,8,13,21,34];
+number_of_wavelets = 6;
+number_of_loops = 10;
 
-
-for i = 1:6
+for i = 1:number_of_wavelets
   disp(char(wnames(i)));
   fprintf('CR BPP snr \n');
   plotdata = [];
   % nbloop - number of loops
-  for nbloop = 1:12
-    [CR,BPP,snr] = get_compresion_rates(X, meth, char(wnames(i)), nbloop);
+  for j = 1:number_of_loops
+    [CR,BPP,snr] = get_compresion_rates(X, meth, char(wnames(i)), nbloops(:,j));
     fprintf('%3.2f, %3.2f, %3.2f \n', CR, BPP, snr);
-    plotdata(:,nbloop) = snr;
+    plotdata(:,j) = snr;
   end
   %figure;plot(plotdata);
-  subplot(6,1,i), plot(plotdata), title(char(wnames(i)));
+  subplot(number_of_wavelets,1,i), plot(plotdata), title(char(wnames(i)));
 end
 
 
