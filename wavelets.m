8dba3343cae12e41719c7b54459f74961fb8b164
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-11-26 02:11:07 -0800

    added result

--- wavelets.m
+++ wavelets.m
@@ -3,8 +3,11 @@ close all;
 clear all;
 load mask;
 
-meth   = 'ezw';   % Method name
+% Method name
+meth   = 'ezw';
+% wavelet names
 wnames = {'haar', 'db1', 'sym2', 'coif1', 'bior1.1', 'rbio1.1'};
+% loop number values
 nbloops = [1,2,3,5,6,7,8,13,21,34];
 number_of_wavelets = 6;
 number_of_loops = 10;
