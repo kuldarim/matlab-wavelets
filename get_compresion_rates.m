94bb94779909ecdd957224033961e1c06e02bb51
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-11-29 10:15:54 -0800

    changed method

--- get_compresion_rates.m
+++ get_compresion_rates.m
@@ -1,9 +1,9 @@
 % CR - compresion rate
 % BPP - bits per pixel
 % snr - snr value
-function [ CR, BPP, snr ] = get_compresion_rates(image, meth, wname, nbloop )
+function [ CR, BPP, snr ] = get_compresion_rates(image, meth, wname, ratio )
 
-[CR,BPP] = wcompress('c',image,'mask.wtc',meth,'maxloop', nbloop, 'wname',wname);
+[CR,BPP] = wcompress('c',image,'mask.wtc',meth,'comprat', ratio, 'wname',wname);
 Xc = wcompress('u','mask.wtc');
 [peaksnr, snr] = psnr(Xc, image);
 
--- wavelets.m
+++ wavelets.m
@@ -3,22 +3,24 @@ close all;
 clear all;
 load mask;
 
+%https://se.mathworks.com/help/wavelet/ref/wcompress.html
 % Method name
-meth   = 'ezw';
+meth   = 'lvl_mmc';
 % wavelet names
 wnames = {'haar', 'db1', 'sym2', 'coif1', 'bior1.1', 'rbio1.1'};
-% loop number values
-nbloops = [1,2,3,5,6,7,8,13,21,34];
+% 'ParName' = 'comprat' or 'COMPRAT' sets the compression ratio.
+% ParVal must be such that 0 ? ParVal ? 100.
+compresion_ratios = [0,10,20,30,40,50,60,70,80,90,100];
 number_of_wavelets = 6;
-number_of_loops = 10;
+number_of_ratios = 11;
 
 for i = 1:number_of_wavelets
   disp(char(wnames(i)));
   fprintf('CR BPP snr \n');
   plotdata = [];
   % nbloop - number of loops
-  for j = 1:number_of_loops
-    [CR,BPP,snr] = get_compresion_rates(X, meth, char(wnames(i)), nbloops(:,j));
+  for j = 1:number_of_ratios
+    [CR,BPP,snr] = get_compresion_rates(X, meth, char(wnames(i)), compresion_ratios(:,j));
     fprintf('%3.2f, %3.2f, %3.2f \n', CR, BPP, snr);
     plotdata(:,j) = snr;
   end
